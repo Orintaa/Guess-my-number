e24d371387dc3358489d5521856e389655fd788d
Orintaa <orinta.strac@gmail.com>
2022-02-28 19:19:00 +0200

    secret Number

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+// console.log(document.querySelector('.message').textContent);
+// document.querySelector('.message').textContent = 'Correct Number!';
+
+// document.querySelector('.number').textContent = 13;
+// document.querySelector('.score').textContent = 20; 
+
+// document.querySelector('.guess').value = 23;
+// console.log(document.querySelector('.guess').value);
+
+// const secretNumber = 3;
+const secretNumber = Math.trunc(Math.random()*20) + 1;
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', () => {
+    const guess = Number(document.querySelector('.guess').value);
+    console.log(guess);
+
+     if (!guess) {
+        document.querySelector('.message').textContent = 'No number!';
+    } else if (guess === secretNumber) {
+        document.querySelector('.message').textContent = '🎉Correct Number!';
+    } else if (guess > secretNumber) {
+        document.querySelector('.message').textContent = '↗️ Too high!'
+    } else if (guess < secretNumber) {
+        document.querySelector('.message').textContent = '↗↘️  Too low!'
+    }
+})
